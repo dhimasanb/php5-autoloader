436c673b4136d77d7b4e322eb4647e75bd5ab550
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-03-17 21:06:06 +0700

    tambah fungsi autoload

--- index.php
+++ index.php
@@ -1,9 +1,12 @@
 <?php
-include 'Printer.php';
+// spl_autoload_register berfungsi menerima parameter closure/fungsi yang memiliki sebuah parameter $class yang berisi nama class yang akan dipanggil. Di dalam closure ini, kita melakukan include ke class yang diinginkan
+spl_autoload_register(function ($class) {
+  include $class . '.php';
+});
+
 $printer = new Printer();
 $buku = $printer->cetakBuku("Nama Buku");
 
-include 'Kurir.php';
 $kurir = new Kurir();
 $kurir->kirim($buku, "Tujuan");
 ?>
